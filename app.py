f958ec12238fea55389eff3639da6c949674f184
mallaaditya16 <50994554+mallaaditya16@users.noreply.github.com>
2021-07-10 23:33:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify
+from flask import request
+from flask_cors import CORS
+from deployment import *
+import pickle
+app=Flask(__name__)
+CORS(app)
+file1=open("allresponses.pkl",'rb')
+answers=pickle.load(file1)
+file1.close()
+@app.route('/api/',methods=["POST"])
+def first():
+    s=request.data
+    s=s.decode("utf-8")
+    #function imported from deployment.py
+    return test(s,answers)
